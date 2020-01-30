b3eb382925dab7b6fa27674512c27cab36570a35
Elyas <elyas0903@gmail.com>
2020-01-29 22:48:49 -0500

    My first commit = Yiliyaziiiii

---
+++ src/NewAment/FirstAS.java
@@ -0,0 +1,41 @@
+package NewAment;
+
+public class FirstAS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// Arithmetic Operators
+		int bk1Age = 22;
+		int bk2Age = 20;
+		int bk3Age = 18;
+		int bk4Age = 16;
+		
+		int addNum = bk1Age + bk2Age + bk3Age + bk4Age;
+		int subNum = bk1Age - bk2Age - bk3Age - bk4Age;
+		int mulNum = bk1Age * bk2Age * bk3Age * bk4Age;
+		int divNum = bk1Age / bk2Age / bk3Age / bk4Age;
+		int modNum = bk1Age%bk4Age;
+		
+		int bkageInc = bk1Age + bk2Age + bk3Age + bk4Age++;
+		int bkageDec = bk1Age - bk2Age - bk3Age - bk4Age--;
+		
+		
+		
+		System.out.println("addNum Results="+ addNum);
+		System.out.println("subNum Results="+ subNum);
+		System.out.println("mulNum Results= "+ mulNum);
+		System.out.println("divNum Results="+ divNum);
+		System.out.println("modNum Results="+modNum);
+		System.out.println("bkageInc Results="+bkageInc);
+		System.out.println("bkageDec Rusults="+bkageDec);
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+
+}
